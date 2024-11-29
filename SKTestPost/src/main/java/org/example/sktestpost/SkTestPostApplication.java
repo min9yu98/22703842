cdf3de8cec9c6b7110d245a78f86c5350cf8ede4
MinGyu <kimmingyu9811@gmail.com>
2024-11-30 01:13:08 +0900

    feat: first commit and project setting

---
+++ SKTestPost/src/main/java/org/example/sktestpost/SkTestPostApplication.java
@@ -0,0 +1,13 @@
+package org.example.sktestpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkTestPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkTestPostApplication.class, args);
+	}
+
+}
---
+++ SKTestPost/src/test/java/org/example/sktestpost/SkTestPostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sktestpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkTestPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
