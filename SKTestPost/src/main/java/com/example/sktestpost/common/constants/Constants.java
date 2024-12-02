680c79b500733c8cff88d8d9b3141105f04dd647
MinGyu <kimmingyu9811@gmail.com>
2024-12-03 03:12:19 +0900

    rafactor: 게시글 목록 조회 검색 분류 소문자로 변경

--- SKTestPost/src/main/java/com/example/sktestpost/common/constants/Constants.java
+++ SKTestPost/src/main/java/com/example/sktestpost/common/constants/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
 	public static final int PAGE_SIZE = 10;
 	public static final String PAGE_SORT_CRITERIA = "createdAt";
-	public static final String TITLE = "TITLE";
-	public static final String MEMBER_ACCOUNT_ID = "MEMBER_ACCOUNT_ID";
+	public static final String TITLE = "title";
+	public static final String MEMBER_ACCOUNT_ID = "member_account_id";
 }
