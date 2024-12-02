789dafc135a786cf58386b6e1eee30cf8845698f
MinGyu <kimmingyu9811@gmail.com>
2024-12-02 20:06:49 +0900

    feat: 게시글 작성 화면, 수정 화면 개발

--- vue-sktestpost/src/components/PageHeader.vue
+++ vue-sktestpost/src/components/PageHeader.vue
@@ -1,6 +1,9 @@
 <!-- PageHeader vue -->
 <template>
     <header>
+        <div id="header-title">
+            <h1>게시판</h1>
+        </div>
         <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link> |
