cb7a39e116d0cab4a4ab7a3a6534d99e39099e61
MinGyu <kimmingyu9811@gmail.com>
2024-12-04 18:33:20 +0900

    refactor: 백엔드 서버 포트 변경 및 cors 프론트 엔드 포트 변경

--- SKTestPost/src/main/java/com/example/sktestpost/common/config/security/SecurityConfig.java
+++ SKTestPost/src/main/java/com/example/sktestpost/common/config/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
 				@Override
 				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 					CorsConfiguration configuration = new CorsConfiguration();
-					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+					configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
 					configuration.setAllowedMethods(Collections.singletonList("*"));
 					configuration.setAllowCredentials(true);
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
